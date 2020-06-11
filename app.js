a5e734a35f2786c669a039b59d19526818953402
chisacam <dladsds123@gmail.com>
2020-06-11 18:13:15 +0900

    cors middleware 적용

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+const cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -36,7 +37,7 @@ const swaggerSpec = swaggerJSDoc(options);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
