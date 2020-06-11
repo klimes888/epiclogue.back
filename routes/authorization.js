6c556c7d6dafbf724383482076fa2fb9da170783
chisacam <dladsds123@gmail.com>
2020-06-11 18:27:12 +0900

    로그인 jwt토큰용 시크릿키 재 추가

--- routes/authorization.js
+++ routes/authorization.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
-const SECRET_KEY = process.env.SECRET_KEY;
 require('dotenv').config();
+const SECRET_KEY = process.env.SECRET_KEY;
+
 const verifyToken = (req, res, next) => {
     try {
         const clientToken = req.cookies.user;
