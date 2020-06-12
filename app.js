c7f660c6dd545b22d0016c5fdffb91cf5ee114c2
chisacam <dladsds123@gmail.com>
2020-06-12 23:33:01 +0900

    주석 제거

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ const swaggerSpec = swaggerJSDoc(options);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.use(cors()); //cors 허용
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
