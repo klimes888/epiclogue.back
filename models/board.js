60e9bcd71f3a73d70ff6a22889bcc249f219f58a
taypark <dodgeplay@naver.com>
2020-07-01 14:26:26 +0900

    부분적 협의 필요

--- models/board.js
+++ models/board.js
@@ -9,12 +9,27 @@ const board = new mongoose.Schema({
     boardBody:{type:String},
     catagory: {type:String, required: true},
     pub: {type:String, required: true},
-    writeDate: {type:Date, required: true},
+    writeDate: {type:Date, required: true, default: Date.now},
     originUid:{type:String, required: true},
     originBuId:{type:String, required: true},
     likeCount:{type:Number},
     replyList:{type:[reply]}
 })
+
+board.static.create = function (data) {
+    const boardData = new this(data);
+
+    return boardData.save();
+}
+
+// 수정, 삭제, 댓글에 필요한 boardId
+board.static.getArticle = function (boardId) {
+
+}
+
+board.static.findAll = function () {
+    return this.find({});
+}
 
 
 
--- routes/board.js
+++ routes/board.js
@@ -1,25 +1,58 @@
-var express = require('express');
-var router = express.Router();
-const {verifyToken} = require('./authorization');
+const express = require("express");
+const router = express.Router();
+const { verifyToken } = require("./authorization");
+require("dotenv").config();
+const Board = require("../models/board");
+const multer = require("multer");
 
-router.post('/posting', verifyToken, async function(req, res, next) {
+/* 투고 */
+router.post("/posting", verifyToken, async function (req, res, next) {
+  const uid = res.locals.uid;
+  const boardTitle = req.body.boardTitle;
+  const boardBody = req.body.boardBody;
+  const boardImg = req.body.boardImg;
+  const category = req.body.category;
+  const pub = req.body.pub;
+  const writeDate = req.body.writeDate;
+  const language = req.body.language;
 
-})
+  for (let i = 0; i < req.file.length; i++) {
+    console.log(`이미지 ${i} 경로 : ${req.files[i].boardImg}`);
+  }
 
-router.post('/editBoard', verifyToken, async function(req, res, next) {
+  const result = await Board.create({
+    boardTitle,
+    boardBody,
+    boardImg,
+    category,
+    pub,
+    writeDate,
+    language
+  });
 
-})
+  if (result) {
+    res.status(201).json({
+      result: 'ok',
+      // 메인으로 리다이렉트
+    })
+  }
 
-router.post('/translate', verifyToken, async function(req, res, next) {
+});
 
-})
+router.post("/editBoard", verifyToken, async function (req, res, next) {
 
-router.post('/comment', verifyToken, async function(req, res, next) {
-  
-})
+});
 
-router.get('/postlist', verifyToken, async function(req,res,next) {
-    
-})
+router.post("/translate", verifyToken, async function (req, res, next) {
+
+});
+
+router.post("/comment", verifyToken, async function (req, res, next) {
+
+});
+
+router.get("/postlist", verifyToken, async function (req, res, next) {
+
+});
 
 module.exports = router;
