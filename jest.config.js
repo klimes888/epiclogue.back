6cfd4dcfbecf1d33652566e5eba9f7255ea090ea
taypark <dodgeplay@naver.com>
2020-09-24 16:54:27 +0900

    feat: Joi 적용 reply test case 추가

--- jest.config.js
+++ jest.config.js
@@ -18,8 +18,8 @@ module.exports = {
     /* integration */
     "./__tests__/integration/v1/user.test.js",
     "./__tests__/integration/v1/board.test.js",
-    // "./__tests__/integration/v1/feedback.test.js",
-    "./__tests__/integration/v1/reply.test.js",
+    "./__tests__/integration/v1/feedback.test.js",
+    // "./__tests__/integration/v1/reply.test.js",
 
     /* interactions */
     "./__tests__/interactions/v1/follow.test.js",
