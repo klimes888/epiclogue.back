89dc692240e83897def0d490f568a5461b450fa3
taypark <dodgeplay@naver.com>
2020-09-25 18:03:24 +0900

    feat: bookmark.ctrl.js 에러 핸들링 추가

--- src/routes/interaction/bookmark/bookmark.ctrl.js
+++ src/routes/interaction/bookmark/bookmark.ctrl.js
@@ -1,5 +1,5 @@
 import { Bookmark, React, Board, User } from '../../../models'
-
+import createError from 'http-errors'
 /* 
   This is bookmark router.
   base url: /interaction/bookmark[?screenId=lunarcat123]
@@ -12,6 +12,7 @@ export const getBookmarkList = async (req, res, next) => {
   try {
     const userId = await User.getIdByScreenId(screenId)
     const bookmarkSet = await Bookmark.getByUserId(userId)
+
     console.log(`[INFO] 유저 ${res.locals.uid}가 ${userId._id}의 북마크 리스트를 확인했습니다.`)
     return res.status(200).json({
       result: 'ok',
@@ -19,10 +20,7 @@ export const getBookmarkList = async (req, res, next) => {
     })
   } catch (e) {
     console.log(`[Error] ${e}`)
-    return res.status(500).json({
-      result: 'error',
-      message: e.message,
-    })
+    return next(createError(500, '알 수 없는 에러가 발생했습니다.'))
   }
 }
 
@@ -43,20 +41,17 @@ export const addBookmark = async function (req, res, next) {
     await React.create(reactData)
     await Board.countBookmark(req.body.boardId, 1)
     await Board.countReact(req.body.boardId, 1)
+
     const bookmarkCount = await Board.getBookmarkCount(req.body.boardId)
 
     console.log(`[INFO] 유저 ${res.locals.uid}가 북마크에 ${req.body.boardId}를 추가했습니다.`)
-
     return res.status(201).json({
       result: 'ok',
       data: bookmarkCount,
     })
   } catch (e) {
     console.log(`[Error] ${e}`)
-    return res.status(500).json({
-      result: 'error',
-      message: e.message,
-    })
+    return next(createError(500, '알 수 없는 에러가 발생했습니다.'))
   }
 }
 
@@ -69,19 +64,16 @@ export const deleteBookmark = async (req, res, next) => {
     await React.delete(userId, boardId)
     await Board.countBookmark(req.body.boardId, 0)
     await Board.countReact(req.body.boardId, 0)
+
     const bookmarkCount = await Board.getBookmarkCount(req.body.boardId)
 
     console.log(`[INFO] 유저 ${res.locals.uid}가 북마크에 ${req.body.boardId}를 해제했습니다.`)
-
     return res.status(200).json({
       result: 'ok',
       data: bookmarkCount,
     })
   } catch (e) {
     console.log(`[Error] ${e}`)
-    return res.status(500).json({
-      result: 'error',
-      message: e.message,
-    })
+    return next(createError(500, '알 수 없는 에러가 발생했습니다.'))
   }
 }
