d1ac33e22143bc5773acc0068d2058f1809eb61e
chisacam <dladsds123@gmail.com>
2020-10-06 19:44:43 +0900

    fix imageUpload import name

--- src/routes/board/index.js
+++ src/routes/board/index.js
@@ -6,14 +6,14 @@ import react from './react'
 import { verifyToken } from '../../lib/middleware/tokenAuth'
 import { checkWriter } from '../../lib/middleware/checkPermission'
 import { checkExistence } from '../../lib/middleware/checkExistence'
-import upload from '../../lib/imageCtrl'
+import {uploadImage} from '../../lib/imageCtrl'
 
 board.get('/', verifyToken, boardCtrl.getBoards)
-board.post('/', verifyToken, upload.any(), boardCtrl.postBoard)
+board.post('/', verifyToken, uploadImage.any(), boardCtrl.postBoard)
 board.get('/:boardId', verifyToken, checkExistence, boardCtrl.viewBoard)
 board.delete('/:boardId', verifyToken, checkExistence, checkWriter, boardCtrl.deleteBoard)
 board.get('/:boardId/edit', verifyToken, checkExistence, checkWriter, boardCtrl.getEditInfo)
-board.post('/:boardId/edit', verifyToken, checkExistence, checkWriter, upload.any(), boardCtrl.postEditInfo)
+board.post('/:boardId/edit', verifyToken, checkExistence, checkWriter, uploadImage.any(), boardCtrl.postEditInfo)
 board.use('/:boardId/feedback', checkExistence, feedback)
 board.use('/:boardId/react', verifyToken, react)
 
--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -2,10 +2,10 @@ import express from 'express'
 const user = express.Router()
 import { verifyToken } from '../../lib/middleware/tokenAuth'
 import * as userCtrl from './user.ctrl'
-import upload from '../../lib/imageCtrl'
+import {uploadImage} from '../../lib/imageCtrl'
 
 user.get('/editProfile', verifyToken, userCtrl.getUserEditInfo)
-user.post('/editProfile', verifyToken, upload.any(), userCtrl.postUserEditInfo)
+user.post('/editProfile', verifyToken, uploadImage.any(), userCtrl.postUserEditInfo)
 user.patch('/changePass', verifyToken, userCtrl.changePass)
 user.delete('/', verifyToken, userCtrl.deleteUser)
 
