1181fefc46be4ca1c361e90074d316e0d8dcf389
taypark <interrupt@kakao.com>
2020-10-23 16:19:57 +0900

    fix TCs

--- __tests__/configs/globalSetup.js
+++ __tests__/configs/globalSetup.js
@@ -5,17 +5,17 @@ import child_process from 'child_process'
 module.exports = async () => {
   // console.log(`mongodb: ${Connection.readyState}`) // normally got 'undefined' while mongodb is running
   /* MongoDB가 servicing이 아닐 때 시작시켜주는 코드입니다. 다만 db health check가 적절하지 않아 매번 동작하므로 리팩토링이 필요합니다. */
-  if (Connection.readyState !== 1) { 
-    console.log(`[FATAL] Database is not servicing or disconnected`)
-    child_process.exec('sudo service mongodb start', (err, stdout, stderr) => {
-      if (err) {
-        console.error(`There's error with starting database ${err}`)
-        console.error(stderr)
-        process.exit(1)
-      }
-      console.log(stdout)
-    })
-  }
+  // if (Connection.readyState !== 1) { 
+  //   console.log(`[FATAL] Database is not servicing or disconnected`)
+  //   child_process.exec('sudo service mongodb start', (err, stdout, stderr) => {
+  //     if (err) {
+  //       console.error(`There's error with starting database ${err}`)
+  //       console.error(stderr)
+  //       process.exit(1)
+  //     }
+  //     console.log(stdout)
+  //   })
+  // }
   
   await Database.connect()
   await Database.drop()
