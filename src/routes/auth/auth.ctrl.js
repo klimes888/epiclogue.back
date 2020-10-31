be133a80c01228d2012369cfbd62453e591a64dc
taypark <interrupt@kakao.com>
2020-10-31 23:53:59 +0900

    edit: separate email text in auth.ctrl.js to sendMail.js

--- src/routes/auth/auth.ctrl.js
+++ src/routes/auth/auth.ctrl.js
@@ -8,8 +8,7 @@ import createError from 'http-errors'
 import { startSession } from 'mongoose'
 const SECRET_KEY = process.env.SECRET_KEY
 const randomBytesPromise = util.promisify(crypto.randomBytes)
-import transporter from '../../lib/sendMail'
-import user from '../../models/user'
+import transporter, { emailText } from '../../lib/sendMail'
 
 /* 
   This is auth router. 
@@ -31,7 +30,8 @@ export const login = async function (req, res, next) {
   })
 
   try {
-    try { // user input validation
+    try {
+      // user input validation
       await loginValidationSchema.validateAsync({ email, userPw })
     } catch (e) {
       console.log(
@@ -104,11 +104,9 @@ export const join = async function (req, res, next) {
     nick: Joi.string().trim().required(),
   })
 
-  const session = await startSession()  
-  await session.startTransaction()
-
   try {
-    try { // user input validation
+    try {
+      // user input validation
       /* 
         validate를 사용하면 try-catch를 사용할 수 없고
         await 를 붙이지 않으면 unhandled promise error 가 나온다...
@@ -126,6 +124,7 @@ export const join = async function (req, res, next) {
           console.log(`[INFO] 중복된 이메일 ${email} 로 가입하려했습니다.`)
           return next(createError(400, '중복된 이메일입니다. 다른 이메일로 가입해주세요.'))
         }
+
         const generatedId = crypto.createHash('sha256').update(email).digest('hex').slice(0, 14)
         const salt = await randomBytesPromise(64)
         const crypt_Pw = crypto.pbkdf2Sync(
@@ -143,93 +142,15 @@ export const join = async function (req, res, next) {
           nickname: nick,
           token: auth_token,
           screenId: generatedId,
-        }, session)
-        
-        throw new Error("tx err")
-        // await session.commitTransaction()
+        })
+
         if (result) {
           const option = {
             from: process.env.MAIL_USER,
             to: email,
             subject: '이메일 인증을 완료해주세요.',
-            html: `
-              <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-              <html html="" xmlns="http://www.w3.org/1999/xhtml" style="margin: 0; padding: 0;">
-              
-              <head>
-                  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-                  <title>Document</title>
-                  
-              </head>
-              
-              <body style="margin: 0; padding: 0; background: #F7F7F7;">
-                  <center class="wrapper" style="margin: 0; padding: 0; width: 100%; table-layout: fixed; background: #2222; padding-bottom: 30px;">
-                      <div class="webkit" style="margin: 0; padding: 0; max-width: 480px; background: #fff;">
-                          <table class="templet" align="center" style="border-collapse: collapse; border-radius: 12px; margin: 0 auto; width: 100%; text-align: center; border-spacing: 0; padding: 20px; font-family: sans-serif; color: #6A6877;" width="100%">
-                              <tr style="margin: 0; padding: 0;">
-                                  <td style="margin: 0; padding: 0;">
-                                      <table width="100%" style="margin: 0; padding: 0; border-collapse: collapse; border-spacing: 0;">
-                                          <tr style="margin: 0; padding: 0;">
-                                              <td style="margin: 0; padding: 0; background: #fff; padding-top: 20px; text-align: center;" align="center">
-                                                  <p style="margin: 0; padding: 0; font-size: 24px; font-weight: 700; color: rgba(21, 146, 230, 0.8);">welcome to EpicLogue</p>
-                                              </td>
-                                          </tr>
-                                      </table>
-                                  </td>
-                              </tr>
-                              <table width="100%" style="margin: 0; padding: 0; border-collapse: collapse; border-spacing: 0;" align="center">
-                                  <tr style="margin: 0; padding: 0;">
-                                      <td style="margin: 0; padding: 0; text-align: center; margin-left: 0 auto;" align="center">
-                                          <p style="margin: 0; padding: 0; font-size: 20px; font-weight: 700; color: rgb(113,113,113); margin-top: 12px; margin-bottom: 18px;">Where imagination comes true!</p>
-              
-              
-                                      </td>
-                                  </tr>
-                              </table>
-                              <tr style="margin: 0; padding: 0;">
-                                  <td style="margin: 0; padding: 0;">
-                                      <p class="contents" style="margin: 0; font-size: 16px; font-weight: 600; color: #6A6877; padding: 12px 0;">더 많은 작품을 공유해보세요</p>
-                                  </td>
-                              </tr>
-                              <tr style="margin: 0; padding: 0;">
-                                  <td style="margin: 0; padding: 0;">
-                                      <p class="contents" style="margin: 0; font-size: 16px; font-weight: 600; color: #6A6877; padding: 12px 0;">더욱 편리한 서비스를 느껴보세요</p>
-                                  </td>
-                              </tr>
-                              <tr style="margin: 0; padding: 0;">
-                                  <td style="margin: 0; padding: 0;">
-                                      <p class="contents" style="margin: 0; font-size: 16px; font-weight: 600; color: #6A6877; padding: 12px 0;">작가와 직접 소통 해보세요</p>
-                                  </td>
-                              </tr>
-                              <tr style="margin: 0; padding: 0;">
-                                  <td style="margin: 0; padding: 0;">
-                                      <div style="padding: 0; border: 2px solid #2222; margin: 25px 40px;"></div>
-                                  </td>
-                              </tr>
-                              <tr style="margin: 0; padding: 0;">
-                                  <td style="margin: 0; padding: 0;">
-                                      <p style="margin: 0; padding: 0; padding-bottom: 30px; font-size: 16px; color: rgba(21, 146, 230, 1); font-weight: 700;">인증버튼을 클릭 하시면 서비스 이용이 가능해요</p>
-                                  </td>
-                              </tr>
-                                              <tr style="margin: 0; padding: 0;">
-                                  <td style="margin: 0; padding: 0;">
-                                      <a href="https://api.epiclogue.tk/auth/mailAuth?email=${email}&token=${auth_token}" style="margin: 0; padding: 0;"><button class="button" style="margin: 0; padding: 0; all: unset; display: inline-block; width: 70%; height: 42px; background: rgba(21, 146, 230, 0.8); border-radius: 25px; font-size: 16px; font-weight: 700; line-height: 42px; text-decoration: none; color: #fff;">인증하기</button></a>
-                                  </td>
-                              </tr>
-                                                              <tr style="margin: 0; padding: 0;">
-                                  <td style="margin: 0; padding: 0;">
-                                      <p style="margin: 0; padding: 40px 0; font-size: 12px; font-weight: 700; color: #A6A4B2;">Designed by Lunarcat</p>
-                                  </td>
-                              </tr>
-                          </table>
-                      </div>
-                  </center>
-              </body>
-              
-              </html>
-            `,
+            html: emailText(email, auth_token),
           }
-
           transporter.sendMail(option, function (error, info) {
             if (error) {
               console.error(
@@ -258,10 +179,7 @@ export const join = async function (req, res, next) {
     }
   } catch (e) {
     console.error(`[ERROR] 알 수 없는 에러가 발생했습니다. ${e}`)
-    await session.abortTransaction()
     return next(createError(500, '알 수 없는 에러가 발생했습니다.'))
-  } finally {
-    session.endSession()
   }
 }
 
@@ -294,7 +212,7 @@ export const test = async (req, res, next) => {
   const userData = {
     email,
     password: userPw,
-    nickname: nick
+    nickname: nick,
   }
 
   const session = await startSession()
@@ -302,7 +220,7 @@ export const test = async (req, res, next) => {
 
   try {
     await session.withTransaction(async () => {
-      const userSchema = new User(userData);
+      const userSchema = new User(userData)
       await userSchema.save({ session })
       throw new Error(`tx error`)
       // return res.sendStatus(201)
@@ -311,10 +229,9 @@ export const test = async (req, res, next) => {
     next(createError(500, e))
   } finally {
     await session.endSession()
-    const failureData = await User.findOne({ email: userData.email }, { email: 1, _id: 0})
+    const failureData = await User.findOne({ email: userData.email }, { email: 1, _id: 0 })
     console.log(failureData)
   }
-  
 
   // try {
   //   await User.create(userData)
