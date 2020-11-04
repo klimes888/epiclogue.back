02ac14274f908e0ef8e7cb85e6e1415006ee4894
taypark <taypark@github.com>
2020-11-04 20:02:42 +0900

    add: tx on follow.ctrl.js and pass test

--- src/routes/interaction/follow/follow.ctrl.js
+++ src/routes/interaction/follow/follow.ctrl.js
@@ -1,5 +1,6 @@
 import { User, Follow } from '../../../models'
 import createError from 'http-errors'
+import { startSession } from 'mongoose'
 
 /* 
   This is follow router.
@@ -15,19 +16,24 @@ export const addFollow = async (req, res, next) => {
 
   try {
     const didFollow = await Follow.didFollow(followData)
-    
-    if(didFollow) {
-        console.log(`[INFO] 유저 ${res.locals.uid} 가 이미 팔로우 한 ${followData.targetUserId} 에 팔로우를 요청했습니다.`)
-        return next(createError(400, '이미 처리된 데이터입니다.'))
+
+    if (didFollow) {
+      console.log(
+        `[INFO] 유저 ${res.locals.uid} 가 이미 팔로우 한 ${followData.targetUserId} 에 팔로우를 요청했습니다.`
+      )
+      return next(createError(400, '이미 처리된 데이터입니다.'))
     }
+    const session = await startSession()
+    await session.withTransaction(async () => {
+      const followSchema = new Follow(followData)
+      await followSchema.save({ session })
+      await User.countFollowing(followData.userId, 1).session(session)
+      await User.countFollower(followData.targetUserId, 1).session(session)
 
-    await Follow.follow(followData)
-    await User.countFollowing(followData.userId, 1)
-    await User.countFollower(followData.targetUserId, 1)
-
-    console.log(`[INFO] 유저 ${res.locals.uid}가 ${followData.targetUserId}를 팔로우합니다.`)
-    return res.status(201).json({
-      result: 'ok',
+      console.log(`[INFO] 유저 ${res.locals.uid}가 ${followData.targetUserId}를 팔로우합니다.`)
+      return res.status(201).json({
+        result: 'ok',
+      })
     })
   } catch (e) {
     console.error(`[Error] ${e}`)
@@ -43,28 +49,35 @@ export const deleteFollow = async (req, res, next) => {
 
   try {
     const didFollow = await Follow.didFollow(followData)
-    
-    if(!didFollow) {
-        console.log(`[INFO] 유저 ${res.locals.uid} 가 팔로우 하지않은 ${followData.targetUserId} 에 팔로우를 요청했습니다.`)
-        return next(createError(400, '이미 처리된 데이터입니다.'))
-    }
 
-    const unfollow = await Follow.unfollow(followData)
-
-    await User.countFollowing(followData.userId, 0)
-    await User.countFollower(followData.targetUserId, 0)
-
-    if (unfollow.ok === 1) {
-      console.log(`[INFO] 유저 ${res.locals.uid}가 ${followData.targetUserId}를 언팔로우했습니다.`)
-      return res.status(200).json({
-        result: 'ok',
-      })
-    } else if (unfollow.ok === 0) {
-      console.error(
-        `[ERROR] 유저 ${res.locals.uid}가 ${followData.targetUserId}에게 한 언팔로우가 실패했습니다: 데이터베이스 질의에 실패했습니다.`
+    if (!didFollow) {
+      console.log(
+        `[INFO] 유저 ${res.locals.uid} 가 팔로우 하지않은 ${followData.targetUserId} 에 팔로우를 요청했습니다.`
       )
-      return next(createError(500, '알 수 없는 에러가 발생했습니다.'))
+      return next(createError(400, '이미 처리된 데이터입니다.'))
     }
+
+    const session = await startSession()
+    await session.withTransaction(async () => {
+      const unfollow = await Follow.unfollow(followData).session(session)
+
+      await User.countFollowing(followData.userId, 0).session(session)
+      await User.countFollower(followData.targetUserId, 0).session(session)
+
+      if (unfollow.ok === 1) {
+        console.log(
+          `[INFO] 유저 ${res.locals.uid}가 ${followData.targetUserId}를 언팔로우했습니다.`
+        )
+        return res.status(200).json({
+          result: 'ok',
+        })
+      } else if (unfollow.ok === 0) {
+        console.error(
+          `[ERROR] 유저 ${res.locals.uid}가 ${followData.targetUserId}에게 한 언팔로우가 실패했습니다: 데이터베이스 질의에 실패했습니다.`
+        )
+        return next(createError(500, '알 수 없는 에러가 발생했습니다.'))
+      }
+    })
   } catch (e) {
     console.error(`[Error] ${e}`)
     return next(createError(500, '알 수 없는 에러가 발생했습니다.'))
@@ -78,26 +91,28 @@ export const getFollow = async (req, res, next) => {
   const dataSet = []
 
   try {
-    const followList =
-      type === 'following'
-        ? await Follow.getFollowingList(userId._id)
-        : await Follow.getFollowerList(userId._id)
+    const session = await startSession(async () => {
+      const followList =
+        type === 'following'
+          ? await Follow.getFollowingList(userId._id).session(session)
+          : await Follow.getFollowerList(userId._id).session(session)
 
-    for (let data of followList) {
-      let temp = await User.getUserInfo(data.targetUserId, {
-        nickname: 1,
-        screenId: 1,
-        intro: 1,
-        profile: 1,
-        _id: 0
+      for (let data of followList) {
+        let temp = await User.getUserInfo(data.targetUserId, {
+          nickname: 1,
+          screenId: 1,
+          intro: 1,
+          profile: 1,
+          _id: 0,
+        }).session(session)
+
+        dataSet.push(temp)
+      }
+      console.log(`[INFO] 유저 ${res.locals.uid} 가 ${userId._id} 의 ${type} 리스트를 확인합니다.`)
+      return res.status(200).json({
+        result: 'ok',
+        data: dataSet,
       })
-
-      dataSet.push(temp)
-    }
-    console.log(`[INFO] 유저 ${res.locals.uid} 가 ${userId._id} 의 ${type} 리스트를 확인합니다.`)
-    return res.status(200).json({
-      result: 'ok',
-      data: dataSet,
     })
   } catch (e) {
     console.error(`[Error] ${e}`)
