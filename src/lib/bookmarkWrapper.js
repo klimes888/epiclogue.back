62e0e706abf299a7fb3bc1f2b60de9ff2ecdb38b
chisacam <dladsds123@gmail.com>
2020-11-10 16:01:21 +0900

    fix object key add error

--- src/lib/bookmarkWrapper.js
+++ src/lib/bookmarkWrapper.js
@@ -5,22 +5,15 @@ export const bookmarkWrapper = async (userId, boardDataSet) => {
   return new Promise(async (resolve, reject) => {
     let resultSet = []
     if (boardDataSet) {
-      const bookmarkSetFromDB = await models.Bookmark.find({ user: userId }, { board: 1, _id: 0 })
-      // boardId만 추출
-      const bookmarkIdSet = bookmarkSetFromDB.map(eachBookmark => {
+      const bookmarkIdSet = await models.Bookmark.find({ user: userId }, { board: 1, _id: 0 }).map(eachBookmark => {
         return eachBookmark.board.toString()
       })
 
-      for (let eachBoardData of boardDataSet) {
-        if (bookmarkIdSet.includes(eachBoardData._id.toString())) {
-          eachBoardData.isBookmarked = true
-        } else {
-          eachBoardData.isBookmarked = false
-        }
-        console.log(eachBoardData)
+      for (const data of boardDataSet) {
+        const eachBoardData = data.toJSON()
+        eachBoardData.isBookmarked = bookmarkIdSet.includes(eachBoardData._id.toString()) ? true : false
         resultSet.push(eachBoardData)
       }
-
       resolve(resultSet)
     } else {
       reject(new Error('데이터가 존재하지 않습니다.'))
