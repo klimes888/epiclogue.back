285dd1e181403e98127c41264a43ab2c05c956f7
chisacam <dladsds123@gmail.com>
2020-11-20 16:17:56 +0900

    hotfix changePass

--- src/routes/user/user.ctrl.js
+++ src/routes/user/user.ctrl.js
@@ -168,13 +168,13 @@ export const changePass = async function (req, res, next) {
             session
           )
 
-          if (changeResult.modifiedCount === 1) {
+          if (changeResult.nModified === 1) {
             console.log(`[INFO] 유저 ${res.locals.uid} 가 비밀번호를 변경했습니다.`)
             return res.status(200).json({
               result: 'ok',
               message: '비밀번호 변경 완료',
             })
-          } else if (changeResult.modifiedCount !== 1) {
+          } else if (changeResult.nModified !== 1) {
             console.error(
               `[ERROR] 유저 ${res.locals.uid} 의 비밀번호 변경이 실패했습니다: 데이터베이스 질의에 실패했습니다.`
             )
