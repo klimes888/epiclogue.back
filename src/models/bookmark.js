5af234bcaa04760659013086f2c4aec4b698c5ba
taypark <interrupt@kakao.com>
2021-03-27 15:38:19 +0900

    fix typoes

--- src/models/bookmark.js
+++ src/models/bookmark.js
@@ -5,7 +5,7 @@ const { ObjectId } = mongoose
 const Bookmark = new mongoose.Schema({
   user: { type: ObjectId, required: true, ref: 'User' },
   board: { type: ObjectId, required: true, ref: 'Board' },
-  craeteAt: { type: Date, default: Date.now },
+  createdAt: { type: Date, default: Date.now },
 })
 
 export default mongoose.model('Bookmark', Bookmark)
