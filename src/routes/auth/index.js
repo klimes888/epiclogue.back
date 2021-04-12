515964f60206f4ef24c21278f7e16448c2bc0230
chisacam <dladsds123@gmail.com>
2021-04-12 14:13:43 +0900

    add logout func, router

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -5,6 +5,7 @@ const auth = Router({ mergeParams: true })
 
 auth.get('/mailAuth', authCtrl.mailAuth)
 auth.post('/login', authCtrl.login)
+auth.get('/logout', authCtrl.logout)
 auth.post('/join', authCtrl.join)
 auth.post('/findpass', authCtrl.mailToFindPass)
 auth.patch('/findpass', authCtrl.findPass)
