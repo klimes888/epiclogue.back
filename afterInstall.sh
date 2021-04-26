9889ebec153cc7552a6a8cbac113dd731ffa2152
chisacam <dladsds123@gmail.com>
2021-04-26 15:51:31 +0900

    change docker-compose cmd for already exist key

--- afterInstall.sh
+++ afterInstall.sh
@@ -3,11 +3,12 @@ cd /home/ubuntu/build
 if [ -f mongodb.key ];
 then
     echo ' ***already exist key!*** '
-    docker-compose restart
+    docker-compose down
+    docker-compose up --build -d
 else
     echo ' ***create new key!*** '
     openssl rand -base64 756 > mongodb.key
     chown 999:999 mongodb.key
     chmod 400 mongodb.key
-    docker-compose up --build -d
+    docker-compose up -d
 fi
